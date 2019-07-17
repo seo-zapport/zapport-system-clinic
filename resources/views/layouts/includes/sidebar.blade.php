56ae5d10510dbc8328ea99f9854951e22817b827
jemsay07 <11234073+jemsay07@users.noreply.github.com>
2019-07-17 14:23:35 +0800

    Update sidebar

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -11,9 +11,9 @@
             <div class="zap-image">
                 @auth()
                 @if (!empty(auth()->user()->employee->profile_img))
-                <img src="{{ asset('storage/uploaded/'.auth()->user()->employee->profile_img) }}" border="0" width="48" class="img-circle">
+                    <img src="{{ asset('storage/uploaded/'.auth()->user()->employee->profile_img) }}" border="0" width="48" class="img-circle">
                 @else
-                <img src="http://sximo5.sximoinc.com/uploads/users/1.jpg" border="0" width="48" class="img-circle">
+                    <img src="{{ url( '/images/default.png' ) }}" border="0" width="48" class="img-circle">
                 @endif
                 @endauth
             </div>
@@ -23,34 +23,40 @@
                 <div class="name">{{ auth()->user()->employee->first_name }}</div>
                 <div class="position">{{ auth()->user()->employee->positions->position }}</div>
                 @else
-                <div class="name">Link your</div>
-                <div class="position">employee ID first.</div>
+                <div class="name">Employee Name</div>
+                <div class="position">Employee Position</div>
                 @endif
                 @endauth
             </div>
         </div>
-        <li class="nav-item"><a href="{{ route('dashboard.main') }}" class="nav-link @yield('overview')">OverView</a></li>
+        <li class="nav-item"><a href="{{ route('dashboard.main') }}" class="nav-link @yield('overview')"><i class="fas fa-tachometer-alt"></i> <span class="collapse-label">Dashboard</span></a></li>
         {{-- For HR --}}
         @if (Gate::check('isAdmin') || Gate::check('isHr'))
-            <li class="nav-item"><a href="{{ route('hr.dep.department') }}" class="nav-link @yield('reg_dep')">Department</a></li>
-            <li class="nav-item"><a href="{{ route('hr.pos.position') }}" class="nav-link @yield('reg_pos')">Position</a></li>
-            <li class="nav-item"><a href="{{ route('hr.employees') }}" class="nav-link @yield('employees')">Employees</a></li>
-            <li class="nav-item"><a href="{{ route('hr.emp.emp_form') }}" class="nav-link @yield('reg_emp')">Add Employee</a></li>
+            <li class="nav-item"><a href="{{ route('hr.dep.department') }}" class="nav-link @yield('reg_dep')"><i class="fas fa-building"></i> <span class="collapse-label">Department</span></a></li>
+            <li class="nav-item"><a href="{{ route('hr.pos.position') }}" class="nav-link @yield('reg_pos')"><i class="fas fa-tasks"></i> <span class="collapse-label">Position</span></a></li>
+            <li class="nav-item"><a href="{{ route('hr.employees') }}" class="nav-link @yield('employees')"><i class="fas fa-users"></i> <span class="collapse-label">Employees</span></a></li>
+            <li class="nav-item"><a href="{{ route('hr.emp.emp_form') }}" class="nav-link @yield('reg_emp')"><i class="fas fa-user-plus"></i> <span class="collapse-label">Add Employee</span></a></li>
         @endif
         {{-- For Admin --}}
         @if(Gate::check('isAdmin'))
-            <li class="nav-item"><a href="{{ route('register') }}" class="nav-link @yield('register')">Register User</a></li>
-            <li class="nav-item"><a href="{{ route('dashboard.userRoles') }}" class="nav-link @yield('userRoles')">User Roles</a></li>
-            <li class="nav-item"><a href="{{ route('dashboard.roles') }}" class="nav-link @yield('roles')">Roles</a></li>
+            <li class="nav-item"><a href="{{ route('register') }}" class="nav-link @yield('register')"><i class="fas fa-user-edit"></i> <span class="collapse-label">Register User</span></a></li>
+            <li class="nav-item"><a href="{{ route('dashboard.userRoles') }}" class="nav-link @yield('userRoles')"><i class="fas fa-user-cog"></i> <span class="collapse-label">User Roles</span></a></li>
+            <li class="nav-item"><a href="{{ route('dashboard.roles') }}" class="nav-link @yield('roles')"><i class="fas fa-cogs"></i> <span class="collapse-label">Roles</span></a></li>
         @endif
         {{-- For All Users --}}
         @auth()
         @if (!empty(auth()->user()->employee))
-            <li class="nav-item"><a href="{{ route('employee', ['employee' => auth()->user()->employee->id]) }}"  class="nav-link @yield('employee')">Profile</a></li>
+            <li class="nav-item"><a href="{{ route('employee', ['employee' => auth()->user()->employee->id]) }}"  class="nav-link @yield('employee')"><span class="collapse-label">Profile</span></a></li>
         @else
-            <li class="nav-item"><a href="{{ route('employees') }}"  class="nav-link @yield('employee')">Profile</a></li>
+            <li class="nav-item"><a href="{{ route('employees') }}"  class="nav-link @yield('employee')"><i class="fas fa-user"></i> <span class="collapse-label">Profile</span></a></li>
         @endif
         @endauth
+        <li class="nav-item" id="collapse-menu">
+            <button type="button" id="collapse-button">
+                <span class="collapse-button-icon"><i class="fas fa-chevron-left"></i></span>
+                <span class="collapse-label">Collapse Menu</span>
+            </button>
+        </li>
     </ul>
 </div>
 <div class="col-md-3 d-none">
@@ -65,7 +71,7 @@
             @endif
 
             <div class="list-group">
-                <a href="{{ route('dashboard.main') }}" class="list-group-item list-group-item-action @yield('overview')">OverView</a>
+                <a href="{{ route('dashboard.main') }}" class="list-group-item list-group-item-action @yield('overview')"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
                 {{-- For HR --}}
                 @if (Gate::check('isAdmin') || Gate::check('isHr'))
                     <a href="{{ route('hr.dep.department') }}" class="list-group-item list-group-item-action @yield('reg_dep')">Department</a>
