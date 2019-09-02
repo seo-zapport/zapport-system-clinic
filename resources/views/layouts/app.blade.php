720bb1b56c46b31c0c595324c9fe9d11aa2426b6
jemsay07 <jem07.say@gmail.com>
2019-09-02 17:34:59 +0800

    Breadcrumbs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,8 +40,14 @@
                 <main id="app" class="primary-site" role="main">
                     <div class="wrap">
                         <div class="container-fluid">
-                            <h2 class="heading-title">@yield('heading-title')</h2>
-                            {{-- @include('layouts.breadcrumbs') --}}
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+                                    <h2 class="heading-title">@yield('heading-title')</h2>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    @include('layouts.breadcrumbs')
+                                </div>                                
+                            </div>
                             @if ( request()->is('dashboard*') || request()->is('hr*') )
                                 @yield('dash-content')
                             @else
