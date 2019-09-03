1f88ee0d1dab68080a5fe90cc5d84c4412d2a429
Lawrence <pagellicious@gmail.com>
2019-09-03 16:34:42 +0800

    update authentication on new User Conrtoller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Gate;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
