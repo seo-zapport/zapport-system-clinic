1d5d2ac4031715a5e099f7b1bb694d7a5a9497cb
jemsay07 <jem07.say@gmail.com>
2020-02-07 15:49:02 +0800

    Update button front

--- resources/views/layouts/front-page/index.blade.php
+++ resources/views/layouts/front-page/index.blade.php
@@ -8,8 +8,8 @@
 		<title>@yield('front-title')</title>
 		<link rel="stylesheet" type="text/css" href="{{ asset('/css/slick.css') }}">
 		<link rel="stylesheet" type="text/css" href="{{ asset('/css/slick-theme.css') }}">
-		<link rel="stylesheet" type="text/css" href="{{ asset('/css/front.css') }}">
 		<link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ asset('/css/front.css') }}">
 		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
 	</head>
 	<body id="page-top" class="zp-front-page" data-spy="scroll" data-target="#mainNav" data-postmodal="@yield('modalStatus')">
